7dda8693c509d279c229b2d39c9448584e729016
Abhishek Ranjan <abhishek.ranjan18oct@gmail.com>
2025-05-28 09:11:40 +0530

    Acess to local storage

---
+++ script.js
@@ -0,0 +1,46 @@
+function loadTodos(){
+    // This function will be used to load the todos from the database
+    const todos = JSON.parse(localStorage.getItem("todos")) || {"todoList" : []};
+    console.log(todos);
+    return todos;
+}
+
+// This function will add the given todo to the local storage
+function addToLocalStorage(todoText){
+    const todos = loadTodos();
+    todos.todoList.push(todoText);
+    localStorage.setItem("todos" , JSON.stringify(todos));
+}
+
+function appendTodoInHtml(todoText){
+    const todoList = document.getElementById("todoList");
+    const todo = document.createElement("li");
+
+    todoList.appendChild(todo);
+}
+
+document.addEventListener("DOMContentLoad" , ()=>{
+    const todoInput = document.getElementById("todoInput");
+
+    const submitButton = document.getElementById("addTodo");
+
+    submitButton.addEventListener("click" , (event)=>{
+        const todoText = todoInput.value;
+        if(todoText = ''){
+            alert("Please write something for todo");
+        }else{
+            addToLocalStorage(todoText);
+            appendTodoInHtml(todoText);
+        }
+    })
+    todoInput.addEventListener("change" , (event)=>{
+        // This call back method is fired everytime there is a change in the input tag
+        const todoText = event.target.value;
+
+        event.target.value = todoText.trim();
+
+        console.log(event.target.value);
+    });
+
+    loadTodos();
+})
